4ab99e38b8fcf02337fda2cdfbc4cc8152d8c823
Sugi <developer.sugiono@gmail.com>
2023-12-14 14:11:57 +0700

    fps counter

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,45 @@
-use piston_window::*;
 use find_folder;
 use fps_counter::FPSCounter;
+use piston_window::*;
 
-mod libs;
-mod sprite;
-mod scene;
 mod camera;
-mod player;
-mod object;
 mod collider;
+mod libs;
+mod object;
+mod player;
+mod scene;
+mod sprite;
 use scene::Scene;
 
 fn main() {
-	let mut window: PistonWindow = WindowSettings::new("2D Platformer", (600, 600))
-			.exit_on_esc(true)
-			.build()
-			.unwrap();
-	window.set_ups(60);
+    let mut window: PistonWindow = WindowSettings::new("2D Platformer", (600, 600))
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    window.set_ups(60);
 
-	let mut fps_counter = FPSCounter::new();
+    let mut fps_counter = FPSCounter::new();
 
-	let assets = find_folder::Search::Kids(1).for_folder("assets").unwrap();
-	let mut glyphs = window.load_font(assets.join("FiraSans-Regular.ttf")).unwrap();
-	let mut scene = Scene::new(assets, &mut window);
+    let assets = find_folder::Search::Kids(1).for_folder("assets").unwrap();
+    let mut glyphs = window
+        .load_font(assets.join("FiraSans-Regular.ttf"))
+        .unwrap();
+    let mut scene = Scene::new(assets, &mut window);
 
-	while let Some(e) = window.next() {
-		scene.update(&e, &mut window);
-		let fps = format!("{} fps", fps_counter.tick().to_string());
-		window.draw_2d(&e, |c, g, device| {
+    let mut fps = String::default();
+
+    while let Some(e) = window.next() {
+        if let Some(_) = &e.update_args() {
+            fps = format!("{} fps", fps_counter.tick().to_string());
+        }
+        scene.update(&e, &mut window);
+        window.draw_2d(&e, |c, g, device| {
             let transform = c.transform.trans(10.0, 25.0);
             text::Text::new_color([0.0, 0.0, 0.0, 1.0], 24)
-			.draw(
-                &fps,
-                &mut glyphs,
-                &c.draw_state,
-                transform, g
-            ).unwrap();
+                .draw(&fps, &mut glyphs, &c.draw_state, transform, g)
+                .unwrap();
 
             glyphs.factory.encoder.flush(device);
         });
-	}
+    }
 }
